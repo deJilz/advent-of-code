f178bee0a6268f2838890abf2e3e7ab74773b6db
condejohn <connor.dejohn@gmail.com>
2022-04-19 19:05:49 -0400

    part one update

--- 2021/days/day03.py
+++ 2021/days/day03.py
@@ -7,15 +7,19 @@ def main():
     array = [x.strip() for x in open(input_path+os.path.basename(__file__).split('.')[0]+'.txt', 'r')]
     
     gamma,eps = '',''
-    cnts = [0]*len(array[0])
     for i in range(0,len(array[0])): # goes through each digit place
-        cnts[i] = sum([int(x[i]) for x in array])
-        if cnts[i] > len(array)/2:
-            gamma += '1'
-            eps += '0'
-        else:
-            gamma += '0'
-            eps += '1'
-    print(str(cnts))
+        gamma += most_common(array,i)
+        eps += least_common(array,i)
     print('Power Consumed' + str(int(gamma,2)*int(eps,2)))
+
+def most_common(array,c):
+    if sum([int(x[c]) for x in array]) > (len(array)/2):
+        return '1'
+    return '0'
+
+def least_common(array,c):
+    if sum([int(x[c]) for x in array]) > (len(array)/2):
+        return '0'
+    return '1'
+
 main()
