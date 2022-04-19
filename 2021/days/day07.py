e4a9c804db3dcdf69771f2042a531a5ee41a38e3
condejohn <connor.dejohn@gmail.com>
2022-04-18 20:50:44 -0400

    ez part one

--- 2021/days/day07.py
+++ 2021/days/day07.py
@@ -1,8 +1,4 @@
-
-#from progress.bar import IncrementalBar
-#bar = IncrementalBar('Processing',max=n) # n is loop duration
-#bar.next()
-#bar.finish()
+# 2021 - Day 7
 
 import os
 
@@ -12,6 +8,11 @@ def main():
     array = [x for x in open(input_path+os.path.basename(__file__).split('.')[0]+'.txt', 'r').readlines()]
     array = list(map(int, array[0].strip().split(",")))
     
+    y = 1000000 # starting cheapest fuel
+    for i in range(min(array),max(array)):
+        f = sum([abs(k-i) for k in array])
+        if f < y:
+            y = f
+    print ("Cheapest option costs " + str(y))
 
-        
 main()
