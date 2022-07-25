a2aa812afbfa150649afd8110bd8bfa8a1455c83
condejohn <connor.dejohn@gmail.com>
2022-07-25 00:26:13 -0400

    both day ones

---
+++ 2020/day01_2.cpp
@@ -0,0 +1,37 @@
+// packages
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+// namespaces
+using namespace std;
+
+/*
+	Connor DeJohn
+	July 24 2022
+	Advent of Code 2020
+*/
+
+int main() {
+	ifstream file;
+	string fname = "day01.txt", str;
+	file.open(fname);
+	
+	vector<int> v = {};
+	while (getline(file, str)){
+		v.push_back(stoi(str));
+    }
+	
+	// loop through array
+	int r = v.size();
+	for (int i = 0; i < r; i++) {
+		for (int k = 0; k < r; k++) {
+			for (int j = 0; j < r; j++) {
+				if ((v.at(i) + v.at(k) + v.at(j)) == 2020) {
+					cout << "[*] Ans: " << (v.at(i) * v.at(k) * v.at(j)) << "\n";
+				}
+			}
+		}
+	}
+}
